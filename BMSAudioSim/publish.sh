8275329e5fc66cb8e4d1e7a3c3a4385729ba1035
Richard <you@example.com>
2025-12-14 20:47:10 +0100

    properly reorganizes assets, improves build

--- BMSAudioSim/publish.sh
+++ BMSAudioSim/publish.sh
@@ -6,13 +6,7 @@ mkdir -p publish/windows
 dotnet publish --os linux -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained
 dotnet publish --os win -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained
 
-cp Assets/common/* publish/linux
-cp Assets/common/* publish/windows
-
-cp Assets/linux/* publish/linux
-cp Assets/windows/* publish/windows
-
-cp bin/Release/net9.0/linux-x64/publish/BMSAudioSim publish/linux
-cp bin/Release/net9.0/win-x64/publish/BMSAudioSim.exe publish/windows
-cp README.md publish/
+cp bin/Release/net9.0/linux-x64/publish/* publish/linux
+cp bin/Release/net9.0/win-x64/publish/* publish/windows
+cp ../README.md publish/
 cd publish && zip -r bmsaudiosim.zip *
